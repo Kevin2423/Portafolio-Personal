7935d6bf2ef21787d3e64097beae99c72fb7c283
Kevin2423 <correo2023@gmail.com>
2023-01-23 23:32:10 -0500

    Tercera modificacion

---
+++ script.js
@@ -0,0 +1,35 @@
+//Funcionamiento que aplica el estilo de la opcion selecionada y quita previamente selecionada 
+function selecionar(link){
+    var opcion = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "Selecionado";
+}
+
+//Funcion que muestra el menu responsive 
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function() { efectoHabilidades () };
+
+//funcion que aplica la animacion de la barra de habilidades 
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
